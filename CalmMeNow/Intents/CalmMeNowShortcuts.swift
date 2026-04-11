a0a680e3230802c861d288059b6b40d0bbc378ac
Thierry De Belder <thierrydebelder@gmail.com>
2026-04-11 11:47:37 +0200

    Reorder Siri phrases to prioritise unambiguous triggers

--- CalmMeNow/Intents/CalmMeNowShortcuts.swift
+++ CalmMeNow/Intents/CalmMeNowShortcuts.swift
@@ -5,11 +5,14 @@ struct CalmMeNowShortcuts: AppShortcutsProvider {
     AppShortcut(
       intent: CalmMeDownIntent(),
       phrases: [
-        "Calm me down with \(.applicationName)",
-        "I'm having a panic attack with \(.applicationName)",
-        "Open emergency calm in \(.applicationName)",
-        "Help me calm down with \(.applicationName)",
+        "Open \(.applicationName)",
         "Start \(.applicationName)",
+        "I'm having a panic attack with \(.applicationName)",
+        "I'm panicking, open \(.applicationName)",
+        "Help me breathe with \(.applicationName)",
+        "Open emergency calm in \(.applicationName)",
+        "Calm me down with \(.applicationName)",
+        "Help me calm down with \(.applicationName)",
       ],
       shortTitle: "Calm Me Down",
       systemImageName: "heart.fill"
